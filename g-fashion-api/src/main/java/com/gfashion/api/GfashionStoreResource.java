6e1f15b7e223b4694dd65d5a4c1166a2247ba51e
grantzou <grantzou.nz@gmail.com>
2020-05-22 16:49:35 +1200

    Gfashion Issue #187: refactoring

--- g-fashion-api/src/main/java/com/gfashion/api/GfashionStoreResource.java
+++ g-fashion-api/src/main/java/com/gfashion/api/GfashionStoreResource.java
@@ -6,6 +6,7 @@ import com.gfashion.domain.store.GfStoreGroup;
 import com.gfashion.domain.store.GfWebsite;
 import com.gfashion.restclient.MagentoStoreClient;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
