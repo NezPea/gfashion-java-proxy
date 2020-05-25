276ab33d2c9617707d826a1d3351edfdd36dfd95
andrewluo.us <andrewluo.us@gmail.com>
2020-05-24 21:50:33 -0700

    fix pipeline error

--- g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/HomepageDynamodbRepositoryImpl.java
+++ g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/HomepageDynamodbRepositoryImpl.java
@@ -4,7 +4,9 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.gfashion.data.entity.homepage.GfCustomizedHomepageEntity;
 import com.gfashion.data.repository.dynamodb.HomepageDynamodbRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class HomepageDynamodbRepositoryImpl implements HomepageDynamodbRepository {
 
     @Autowired
