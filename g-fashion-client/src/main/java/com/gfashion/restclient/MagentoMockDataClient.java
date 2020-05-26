9bdfc9bdc1f1e2caf7eccb0e53e3c885e5d6d790
andrewluo.us <andrewluo.us@gmail.com>
2020-05-25 19:14:01 -0700

    Polish Code and Add One Recommended Product

--- g-fashion-client/src/main/java/com/gfashion/restclient/MagentoMockDataClient.java
+++ g-fashion-client/src/main/java/com/gfashion/restclient/MagentoMockDataClient.java
@@ -36,79 +36,60 @@ public class MagentoMockDataClient {
 
     private List<HomepageProduct> getDefaultHomepageProducts(){
         List<HomepageProduct> homepageProducts = new ArrayList<HomepageProduct>();
-        HomepageProduct homepageProduct1 = new HomepageProduct(1, "product1",
-                "http://image1.gf.com", true);
-        HomepageProduct homepageProduct2 = new HomepageProduct(2, "product2",
-                "http://image2.gf.com", false);
-        HomepageProduct homepageProduct3 = new HomepageProduct(3, "product3",
-                "http://image3.gf.com", false);
-        HomepageProduct homepageProduct4 = new HomepageProduct(4, "product4",
-                "http://image4.gf.com", false);
-        HomepageProduct homepageProduct5 = new HomepageProduct(5, "product5",
-                "http://image5.gf.com", false);
-        homepageProducts.add(homepageProduct1);
-        homepageProducts.add(homepageProduct2);
-        homepageProducts.add(homepageProduct3);
-        homepageProducts.add(homepageProduct4);
-        homepageProducts.add(homepageProduct5);
+        homepageProducts.add(HomepageProduct.builder().id(1).name("product1").photoUrl("http://image1.gf.com").isFeatured(true).build());
+        homepageProducts.add(HomepageProduct.builder().id(2).name("product2").photoUrl("http://image2.gf.com").isFeatured(true).build());
+        homepageProducts.add(HomepageProduct.builder().id(3).name("product3").photoUrl("http://image3.gf.com").isFeatured(false).build());
+        homepageProducts.add(HomepageProduct.builder().id(4).name("product4").photoUrl("http://image4.gf.com").isFeatured(false).build());
+        homepageProducts.add(HomepageProduct.builder().id(5).name("product5").photoUrl("http://image5.gf.com").isFeatured(false).build());
+        homepageProducts.add(HomepageProduct.builder().id(6).name("product6").photoUrl("http://image6.gf.com").isFeatured(false).build());
         return homepageProducts;
     }
 
     private List<HomepageBrand> getDefaultHomepageBrands() {
         List<HomepageBrand> homepageBrands = new ArrayList<>();
-        HomepageBrand homepageBrand1 = new HomepageBrand(1, "Barmain", "France", "http://image1.gf.com", "http://gucci.com");
-        HomepageBrand homepageBrand2 = new HomepageBrand(2, "HERMES", "France", "http://image2.gf.com", "http://hermes.com");
-        HomepageBrand homepageBrand3 = new HomepageBrand(3, "CHANEL", "France", "http://image3.gf.com", "http://lv.com");
-        HomepageBrand homepageBrand4 = new HomepageBrand(4, "CHAUMET", "France", "http://image4.gf.com", "http://armani.com");
-        HomepageBrand homepageBrand5 = new HomepageBrand(5, "Brioni", "Italy", "http://image4.gf.com", "http://armani.com");
-
-        homepageBrands.add(homepageBrand1);
-        homepageBrands.add(homepageBrand2);
-        homepageBrands.add(homepageBrand3);
-        homepageBrands.add(homepageBrand4);
-        homepageBrands.add(homepageBrand5);
+        homepageBrands.add(HomepageBrand.builder().id(1).name("Barmain").country("France").photoUrl("http://image1.gf.com").link("http://gucci.com").build());
+        homepageBrands.add(HomepageBrand.builder().id(2).name("HERMES").country("France").photoUrl("http://image2.gf.com").link("http://hermes.com").build());
+        homepageBrands.add(HomepageBrand.builder().id(3).name("CHANEL").country("France").photoUrl("http://image3.gf.com").link("http://lv.com").build());
+        homepageBrands.add(HomepageBrand.builder().id(4).name("CHAUMET").country("France").photoUrl("http://image4.gf.com").link("http://armani.com").build());
+        homepageBrands.add(HomepageBrand.builder().id(5).name("Brioni").country("Italy").photoUrl("http://image5.gf.com").link("http://armani.com").build());
         return homepageBrands;
     }
 
     private List<HomepageDesigner> getDefaultHomepageDesigners() {
         List<HomepageDesigner> homepageDesigners = new ArrayList<>();
-        HomepageDesigner homepageDesigner1 = new HomepageDesigner(1, "Miles", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "HUAXIA", "../../../assets/images/designer1.png");
-        HomepageDesigner homepageDesigner2 = new HomepageDesigner(2, "Snow", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "HUAXIA", "../../../assets/images/designer2.png");
-        HomepageDesigner homepageDesigner3 = new HomepageDesigner(3, "Trump", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "../../../assets/images/designer3.png");
-        HomepageDesigner homepageDesigner4 = new HomepageDesigner(4, "Banon", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "../../../assets/images/designer4.png");
-        HomepageDesigner homepageDesigner5 = new HomepageDesigner(5, "Pompeo", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "../../../assets/images/designer5.png");
-        HomepageDesigner homepageDesigner6 = new HomepageDesigner(6, "Luther", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "HUAXIA", "../../../assets/images/designer6.png");
-        HomepageDesigner homepageDesigner7 = new HomepageDesigner(7, "Pence", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "../../../assets/images/designer1.png");
-        HomepageDesigner homepageDesigner8 = new HomepageDesigner(8, "Ivanka", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "../../../assets/images/designer2.png");
-
-        homepageDesigners.add(homepageDesigner1);
-        homepageDesigners.add(homepageDesigner2);
-        homepageDesigners.add(homepageDesigner3);
-        homepageDesigners.add(homepageDesigner4);
-        homepageDesigners.add(homepageDesigner5);
-        homepageDesigners.add(homepageDesigner6);
-        homepageDesigners.add(homepageDesigner7);
-        homepageDesigners.add(homepageDesigner8);
+        homepageDesigners.add(HomepageDesigner.builder().id(1).name("Miles").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("HUAXIA").photoUrl("../../../assets/images/designer1.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(2).name("Snow").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("HUAXIA").photoUrl("../../../assets/images/designer2.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(3).name("Trump").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("../../../assets/images/designer3.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(4).name("Banon").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("../../../assets/images/designer4.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(5).name("Pompeo").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("../../../assets/images/designer5.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(6).name("Luther").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("HUAXIA").photoUrl("../../../assets/images/designer6.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(7).name("Pence").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("../../../assets/images/designer1.png").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(8).name("Ivanka").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("../../../assets/images/designer2.png").build());
         return homepageDesigners;
     }
 
     private List<HomepageBrand> getDefaultFollowingBrands() {
         List<HomepageBrand> homepageBrands = new ArrayList<>();
-        HomepageBrand homepageBrand1 = new HomepageBrand(1, "Gucci", "France", "http://image1.gf.com", "http://gucci.com");
-
-        homepageBrands.add(homepageBrand1);
+        homepageBrands.add(HomepageBrand.builder().id(1).name("Gucci").country("France").photoUrl("http://image1.gf.com").link("http://gucci.com").build());
         return homepageBrands;
     }
 
     private List<HomepageDesigner> getDefaultFollowingDesigners() {
         List<HomepageDesigner> homepageDesigners = new ArrayList<>();
-        HomepageDesigner homepageDesigner1 = new HomepageDesigner(1, "Miles", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "HUAXIA", "http://image6.gf.com");
-        HomepageDesigner homepageDesigner2 = new HomepageDesigner(2, "Snow", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "HUAXIA", "http://image7.gf.com");
-        HomepageDesigner homepageDesigner3 = new HomepageDesigner(3, "Trump", new ArrayList<String>(Arrays.asList(new String[]{"BALMAIN"})), "USA", "http://image8.gf.com");
-
-        homepageDesigners.add(homepageDesigner1);
-        homepageDesigners.add(homepageDesigner2);
-        homepageDesigners.add(homepageDesigner3);
+        homepageDesigners.add(HomepageDesigner.builder().id(1).name("Miles").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("HUAXIA").photoUrl("http://image6.gf.com").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(2).name("Snow").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("HUAXIA").photoUrl("http://image7.gf.com").build());
+        homepageDesigners.add(HomepageDesigner.builder().id(3).name("Trump").
+                cooperatingBrands(new ArrayList<>(Arrays.asList(new String[]{"BALMAIN"}))).country("USA").photoUrl("http://image8.gf.com").build());
         return homepageDesigners;
     }
 }
