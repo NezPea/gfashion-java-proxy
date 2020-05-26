6eacf2aef683a528630f16700d5be77bacb5c752
grantzou <grantzou.nz@gmail.com>
2020-05-26 13:23:58 +1200

    Revert "AWS NPE fix attempt to shutdown IdleConnectionReaper"
    This reverts commit 2defc39c

--- g-fashion-app/src/main/java/com/gfashion/config/DynamoDBConfig.java
+++ g-fashion-app/src/main/java/com/gfashion/config/DynamoDBConfig.java
@@ -8,15 +8,11 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PreDestroy;
-
 @Configuration
-@Slf4j
 public class DynamoDBConfig {
 
     @Value("${amazon.dynamodb.accesskey}")
@@ -27,10 +23,11 @@ public class DynamoDBConfig {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard()
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accesskey, secretkey)))
                 .withRegion(Regions.US_EAST_1)
                 .build();
+        return client;
     }
 
     @Bean
@@ -41,16 +38,5 @@ public class DynamoDBConfig {
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
         return new DynamoDBMapper(amazonDynamoDB(), DynamoDBMapperConfig.DEFAULT);
-    }
-
-    @PreDestroy
-    public void cleanUp() {
-        try {
-            // Shutting down AWS IdleConnectionReaper thread...
-            com.amazonaws.http.IdleConnectionReaper.shutdown();
-        } catch (Throwable t) {
-            // log error
-            log.error("Shutting down IdleConnectionReaper");
-        }
     }
 }
