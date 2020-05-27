4fc0f7fbf8a2a0bb14377bcaf19f7e074bc1995b
andrewluo.us <andrewluo.us@gmail.com>
2020-05-26 23:16:18 -0700

    Improve Customer Return Model for Get API

--- g-fashion-domain/src/main/java/com/gfashion/domain/customer/GfCustomerAddress.java
+++ g-fashion-domain/src/main/java/com/gfashion/domain/customer/GfCustomerAddress.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.json.JSONObject;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -13,15 +14,23 @@ import java.util.List;
 public class GfCustomerAddress {
     private Integer id;
     private Integer customer_id;
-    private JSONObject region;
+    private GfCustomerRegion region;
     private Integer region_id;
     private String country_id;
     private List<String> street;
+    private String company;
     private String telephone;
+    private String fax;
     private String postcode;
     private String city;
     private String firstname;
     private String lastname;
+    private String middlename;
+    private String prefix;
+    private String suffix;
+    private String vat_id;
     private Boolean default_shipping;
     private Boolean default_billing;
+    private JSONObject extension_attributes;
+    private List<Map<String, String>> custom_attributes;
 }
