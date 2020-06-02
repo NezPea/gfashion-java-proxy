53ab7b6307a52974ea5aecf3b9343ef258e6e61a
Candy_xd <serezhautliakov1999@gmail.com>
2020-06-02 22:26:31 +0800

    update product api

--- g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/DesignerRepositoryImpl.java
+++ g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/DesignerRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.gfashion.data.repository.dynamodb.impl;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 import com.gfashion.data.GfDesignerEntity;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
@@ -27,6 +29,17 @@ public class DesignerRepositoryImpl implements GfDesignerRepository {
 
     @Override
     public GfDesignerEntity readGfDesignerEntity(String DesignerId) {
+        Map<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+        eav.put(":val1", new AttributeValue().withS("USA"));
+//        eav.put(":val2", new AttributeValue().withS(country));
+
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("country = :val1").withExpressionAttributeValues(eav);
+
+        List<GfDesignerEntity> scanResult = dynamoDBMapper.parallelScan(GfDesignerEntity.class, scanExpression,16);
+        for (GfDesignerEntity bicycle : scanResult) {
+            System.out.println(bicycle);
+        }
         return dynamoDBMapper.load(GfDesignerEntity.class, DesignerId);
     }
 
--- g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/ProductRepositoryImpl.java
+++ g-fashion-data/src/main/java/com/gfashion/data/repository/dynamodb/impl/ProductRepositoryImpl.java
@@ -1,19 +1,11 @@
 package com.gfashion.data.repository.dynamodb.impl;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.gfashion.data.GfProductEntity;
 import com.gfashion.data.repository.dynamodb.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.gfashion.data.repository.dynamodb.constant.ProductConstant.PRODUCT_KEY;
 
 @Component
 public class ProductRepositoryImpl implements ProductRepository {
@@ -33,22 +25,32 @@ public class ProductRepositoryImpl implements ProductRepository {
     }
 
     @Override
+    /**
+     * updated by Candy:
+     * https://docs.aws.amazon.com/zh_cn/amazondynamodb/latest/developerguide/DynamoDBMapper.OptionalConfig.html
+     */
     public GfProductEntity updateGfProductEntity(GfProductEntity product) {
-        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
-        expectedAttributeValueMap.put(PRODUCT_KEY, new ExpectedAttributeValue(new AttributeValue().withS(product.getId())));
-        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression().withExpected(expectedAttributeValueMap);
-        dynamoDBMapper.save(product, saveExpression);
+//        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
+//        expectedAttributeValueMap.put(PRODUCT_KEY, new ExpectedAttributeValue(new AttributeValue().withS(product.getId())));
+//        DynamoDBSaveExpression saveExpression = new DynamoDBSaveExpression().withExpected(expectedAttributeValueMap);
+//        dynamoDBMapper.save(product, saveExpression);
+        dynamoDBMapper.save(product, DynamoDBMapperConfig.SaveBehavior.UPDATE_SKIP_NULL_ATTRIBUTES.config());
         return product;
     }
 
     @Override
+    /**
+     * updated by Candy
+     * https://docs.aws.amazon.com/zh_cn/amazondynamodb/latest/developerguide/DynamoDBMapper.CRUDExample1.html
+     */
     public void deleteGfProductEntity (String productId) {
-        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
-        expectedAttributeValueMap.put(PRODUCT_KEY, new ExpectedAttributeValue(new AttributeValue().withS(productId)));
-        DynamoDBDeleteExpression deleteExpression = new DynamoDBDeleteExpression().withExpected(expectedAttributeValueMap);
-        GfProductEntity product = GfProductEntity.builder()
-                .id(productId)
-                .build();
-        dynamoDBMapper.delete(product, deleteExpression);
+//        Map<String, ExpectedAttributeValue> expectedAttributeValueMap = new HashMap<>();
+//        expectedAttributeValueMap.put(PRODUCT_KEY, new ExpectedAttributeValue(new AttributeValue().withS(productId)));
+//        DynamoDBDeleteExpression deleteExpression = new DynamoDBDeleteExpression().withExpected(expectedAttributeValueMap);
+//        GfProductEntity product = GfProductEntity.builder()
+//                .id(productId)
+//                .build();
+        GfProductEntity product = dynamoDBMapper.load(GfProductEntity.class, productId);
+        dynamoDBMapper.delete(product);
     }
 }
