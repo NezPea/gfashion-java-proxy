93f59d919697a99df99db55a1ef5a868a5c1b4a2
Wayne <Wayne@example.com>
2020-06-06 15:31:12 +0800

    实现 magento shipment 相关的 rest api

--- g-fashion-api/src/main/java/com/gfashion/api/GfashionOrderResource.java
+++ g-fashion-api/src/main/java/com/gfashion/api/GfashionOrderResource.java
@@ -6,34 +6,27 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping(path = "/gfashion/v1", produces = {"application/json"})
 //@CrossOrigin(origins = "*")
 @AllArgsConstructor
-public class GfashionShipmentResource {
-	private MagentoShipmentClient magentoShipmentClient;
+public class GfashionOrderResource {
+	private MagentoShipmentClient magentoSalesClient;
 
-	@PostMapping("/shipment")
-	public ResponseEntity<GfShipment> updateShipment(@RequestBody @Validated GfShipment gfShipment) {
+	@PostMapping("/ship")
+	public ResponseEntity<GfShipment> createShipment(@RequestBody @Validated GfShipment gfShipment) {
 		try {
-			gfShipment = magentoShipmentClient.createShipment(gfShipment);
+			gfShipment = magentoSalesClient.createShipment(gfShipment);
 			return ResponseEntity.status(HttpStatus.CREATED).body(gfShipment);
 		} catch (Exception e) {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 	}
-
-	@GetMapping("/shipment/{shipmentId}")
-	public ResponseEntity<GfShipment> getShipmentById(@PathVariable Integer shipmentId) {
-		try {
-			return ResponseEntity.status(HttpStatus.OK).body(magentoShipmentClient.getShipmentById(shipmentId));
-		} catch (Exception e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
-		}
-	}
-
 
 }
--- g-fashion-client/src/main/java/com/gfashion/restclient/MagentoShipmentClient.java
+++ g-fashion-client/src/main/java/com/gfashion/restclient/MagentoShipmentClient.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MagentoSalesClient {
+public class MagentoShipmentClient {
 
     @Value("${magento.url.shipment}")
     private String shipmentUrl;
