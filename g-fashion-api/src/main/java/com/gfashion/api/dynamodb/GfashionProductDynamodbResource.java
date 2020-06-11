b13c5b7cd270bd01649d581dc81046043f4cdca5
eric <ericback2019@protonmail.com>
2020-06-11 12:04:02 +0800

    using 'id' as primary key.

--- g-fashion-api/src/main/java/com/gfashion/api/dynamodb/GfashionProductDynamodbResource.java
+++ g-fashion-api/src/main/java/com/gfashion/api/dynamodb/GfashionProductDynamodbResource.java
@@ -48,7 +48,7 @@ public class GfashionProductDynamodbResource {
     @PutMapping(value = "/dynamodb/products", produces = "application/json;charset=utf-8")
     public ResponseEntity<GfProductEntity> updateProduct(@RequestBody GfProductEntity product) {
         try {
-            if (productRepository.readGfProductEntityById(product.getMagentoId()) == null) {
+            if (productRepository.readGfProductEntityById(product.getId()) == null) {
                 return ResponseEntity.status(HttpStatus.OK).body(null);
             }
             GfProductEntity response = productRepository.updateGfProductEntity(product);
