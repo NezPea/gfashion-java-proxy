9b4a9b3c312e4f473e362e73edc2ceda06f6b37d
mzm1928@gmail.com <mzm1928@gmail.com>
2020-06-12 16:17:04 +0800

    完善支付功能

--- g-fashion-api/src/main/java/com/gfashion/api/GfashionPaymentResource.java
+++ g-fashion-api/src/main/java/com/gfashion/api/GfashionPaymentResource.java
@@ -1,33 +1,77 @@
 package com.gfashion.api;
 
 
-import com.gfashion.domain.payment.GfAdditonal;
+import com.gfashion.domain.payment.GfShipping;
+import com.gfashion.domain.payment.ShippingAddress;
+import com.gfashion.restclient.MagentoCartClient;
 import com.gfashion.restclient.MagentoPaymentClient;
-import com.gfashion.restclient.magento.exception.PaymentNotFoundException;
-import com.gfashion.restclient.magento.exception.PaymentUnknowException;
+import com.gfashion.restclient.magento.exception.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 @RequestMapping(path = "/gfashion/v1/payment", produces = {"application/json"})
-//@CrossOrigin(origins = "*")
 @AllArgsConstructor
 public class GfashionPaymentResource {
-    private MagentoPaymentClient magentoPaymentClient;
+    private final MagentoPaymentClient magentoPaymentClient;
 
     /**
-     * 支付订单生成
+     * 获取支付方式
+     * @param
+     * @return
+     */
+    @PostMapping("/list")
+    public ResponseEntity<String> getPaymentMethods(@RequestBody GfShipping gfShipping) {
+        try {
+            String paymentMethods = magentoPaymentClient.getPaymentMethods(gfShipping);
+            return ResponseEntity.status(HttpStatus.OK).body(paymentMethods);
+        } catch (CustomerException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getErrorMessage());
+        } catch (PaymentNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getErrorMessage());
+        } catch (PaymentUnknowException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getErrorMessage());
+        }
+
+    }
+
+    /**
+     * 获取密钥
+     * @param customerToken
+     * @return
+     */
+    @GetMapping("/getOriginKey")
+    public ResponseEntity<String> getOriginKey(@RequestParam String customerToken) {
+        try {
+            String originKey = magentoPaymentClient.getOriginKey(customerToken);
+            return ResponseEntity.status(HttpStatus.OK).body(originKey);
+        } catch (CustomerException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getErrorMessage());
+        } catch (PaymentNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getErrorMessage());
+        } catch (PaymentUnknowException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getErrorMessage());
+        }
+    }
+
+    /**
+     * 订单生成
      * @return
      */
     @PostMapping("/information")
-    public ResponseEntity<String> paymentInformation(@RequestBody GfAdditonal gfAdditonal) {
+    public ResponseEntity<String> paymentInformation(@RequestParam String encryptionData, @RequestParam String customerToken) {
         try {
-            String orderId = magentoPaymentClient.createPayment(gfAdditonal);
+            String orderId = magentoPaymentClient.createPayment(encryptionData, customerToken);
             return ResponseEntity.status(HttpStatus.OK).body(orderId);
+        } catch (CustomerException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getErrorMessage());
         } catch (PaymentNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getErrorMessage());
         } catch (PaymentUnknowException e) {
@@ -39,10 +83,30 @@ public class GfashionPaymentResource {
      * 查询支付订单状态
      */
     @GetMapping("/{orderId}/status")
-    public ResponseEntity<String> getPaymentStatus(@PathVariable String orderId) {
+    public ResponseEntity<String> getPaymentStatus(@PathVariable String orderId, @RequestParam String customerToken) {
         try {
-            String status = magentoPaymentClient.getPaymentStatus(orderId);
+            String status = magentoPaymentClient.getPaymentStatus(orderId, customerToken);
             return ResponseEntity.status(HttpStatus.OK).body(status);
+        } catch (CustomerException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getErrorMessage());
+        } catch (PaymentNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getErrorMessage());
+        } catch (PaymentUnknowException e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getErrorMessage());
+        }
+    }
+
+    /**
+     * 根据订单状态返回是否threeDS2:true,再次调用验证身份指纹识别
+     * @return
+     */
+    @PostMapping("/verificationFingerprint")
+    public ResponseEntity<String> verification(@RequestParam String fingerprintData, @RequestParam String customerToken) {
+        try {
+            String status = magentoPaymentClient.verificationFingerprint(fingerprintData, customerToken);
+            return ResponseEntity.status(HttpStatus.OK).body(status);
+        } catch (CustomerException e) {
+            throw new ResponseStatusException(e.getStatus(), e.getErrorMessage());
         } catch (PaymentNotFoundException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getErrorMessage());
         } catch (PaymentUnknowException e) {
--- g-fashion-client/src/main/java/com/gfashion/restclient/MagentoPaymentClient.java
+++ g-fashion-client/src/main/java/com/gfashion/restclient/MagentoPaymentClient.java
@@ -1,11 +1,19 @@
 package com.gfashion.restclient;
 
+import com.gfashion.domain.cart.GfCart;
 import com.gfashion.domain.payment.GfAdditonal;
+import com.gfashion.domain.payment.GfShipping;
+import com.gfashion.restclient.magento.cart.MagentoCart;
+import com.gfashion.restclient.magento.exception.CustomerException;
 import com.gfashion.restclient.magento.exception.PaymentNotFoundException;
 import com.gfashion.restclient.magento.exception.PaymentUnknowException;
+import com.gfashion.restclient.magento.mapper.GfMagentoConverter;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -15,18 +23,95 @@ import org.springframework.web.client.HttpStatusCodeException;
 @Slf4j
 public class MagentoPaymentClient {
 
+    @Value("${magento.url.paymentMethods}")
+    private String paymentMethodsUrl;
+
+    @Value("${magento.url.carts}")
+    private String cartsUrl;
+
+    @Value("${magento.url.originKey}")
+    private String originKeyUrl;
+
     @Value("${magento.url.information}")
     private String informationUrl;
 
     @Value("${magento.url.paymentStatus}")
     private String paymentStatusUrl;
 
+    @Value(("${magento.url.verificationFingerprint}"))
+    private String verificationFingerprintUrl;
+
     @Autowired
     private RestClient restClient;
 
-    public String createPayment(GfAdditonal gfAdditonal) throws PaymentNotFoundException, PaymentUnknowException{
+
+    private final GfMagentoConverter mapper = Mappers.getMapper(GfMagentoConverter.class);
+
+    public String getPaymentMethods(GfShipping gfShipping) throws CustomerException, PaymentNotFoundException, PaymentUnknowException {
         try {
-            ResponseEntity<String> responseEntity = restClient.postForEntity(informationUrl, gfAdditonal, String.class, null);
+            HttpHeaders headers = restClient.getCustomerHeaders(gfShipping.getCustomerToken());
+            headers.set("Authorization", "Bearer " + gfShipping.getCustomerToken());
+
+            ResponseEntity<String> responseEntity = restClient.postForEntity(cartsUrl, null, String.class, headers);
+            gfShipping.setCartId(String.valueOf(responseEntity.getBody()));
+            ResponseEntity<String> responseEntityPaymentMethods = restClient.postForEntity(paymentMethodsUrl, gfShipping, String.class, headers);
+            log.info(responseEntityPaymentMethods.getBody());
+            return responseEntityPaymentMethods.getBody();
+
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                throw new CustomerException(e.getStatusCode(), e.getMessage());
+            }
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new PaymentNotFoundException(e.getMessage());
+            }
+            throw new PaymentUnknowException(e.getMessage());
+        }
+    }
+
+    public String getOriginKey(String customerToken)  throws CustomerException, PaymentNotFoundException, PaymentUnknowException{
+        try {
+            HttpHeaders headers = restClient.getCustomerHeaders(customerToken);
+            headers.set("Authorization", "Bearer " + customerToken);
+            ResponseEntity<String> responseEntity = restClient.exchangeGet(originKeyUrl, String.class, headers);
+            log.info(responseEntity.getBody());
+            return responseEntity.getBody();
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                throw new CustomerException(e.getStatusCode(), e.getMessage());
+            }
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new PaymentNotFoundException(e.getMessage());
+            }
+            throw new PaymentUnknowException(e.getMessage());
+        }
+
+    }
+
+    public String createPayment(String encryptionData, String customerToken) throws CustomerException, PaymentNotFoundException, PaymentUnknowException{
+        try {
+            HttpHeaders headers = restClient.getCustomerHeaders(customerToken);
+            headers.set("Authorization", "Bearer " + customerToken);
+            ResponseEntity<String> responseEntity = restClient.postForEntity(informationUrl, encryptionData, String.class, headers);
+            log.info(responseEntity.getBody());
+            return responseEntity.getBody();
+        } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                throw new CustomerException(e.getStatusCode(), e.getMessage());
+            }
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new PaymentNotFoundException(e.getMessage());
+            }
+            throw new PaymentUnknowException(e.getMessage());
+        }
+    }
+
+    public String getPaymentStatus(String orderId, String customerToken) throws CustomerException, PaymentNotFoundException, PaymentUnknowException {
+        try {
+            String url = String.format(paymentStatusUrl, orderId);
+            HttpHeaders headers = restClient.getCustomerHeaders(customerToken);
+            headers.set("Authorization", "Bearer " + customerToken);
+            ResponseEntity<String> responseEntity = restClient.exchangeGet(url, String.class, headers);
             log.info(responseEntity.getBody());
             return responseEntity.getBody();
         } catch (HttpStatusCodeException e) {
@@ -37,13 +122,17 @@ public class MagentoPaymentClient {
         }
     }
 
-    public String getPaymentStatus(String orderId) throws PaymentNotFoundException, PaymentUnknowException {
+    public String verificationFingerprint(String fingerprintData, String customerToken) throws CustomerException, PaymentNotFoundException, PaymentUnknowException {
         try {
-            String url = String.format(paymentStatusUrl, orderId);
-            ResponseEntity<String> responseEntity = restClient.exchangeGet(url, String.class, null);
+            HttpHeaders headers = restClient.getCustomerHeaders(customerToken);
+            headers.set("Authorization", "Bearer " + customerToken);
+            ResponseEntity<String> responseEntity = restClient.postForEntity(informationUrl, fingerprintData, String.class, headers);
             log.info(responseEntity.getBody());
             return responseEntity.getBody();
         } catch (HttpStatusCodeException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                throw new CustomerException(e.getStatusCode(), e.getMessage());
+            }
             if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
                 throw new PaymentNotFoundException(e.getMessage());
             }
