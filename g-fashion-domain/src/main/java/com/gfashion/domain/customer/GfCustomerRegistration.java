42f4360f1b9352b20dc31a2f75dc761eff462109
Andrew Luo <andrewluo.us@gmail.com>
2020-06-13 05:47:09 +0000

    Polish Customer Code
    -Add Address APIs

--- g-fashion-domain/src/main/java/com/gfashion/domain/customer/GfCustomerRegistration.java
+++ g-fashion-domain/src/main/java/com/gfashion/domain/customer/GfCustomerRegistration.java
@@ -1,12 +1,14 @@
 package com.gfashion.domain.customer;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GfCustomerRegistration {
 
     private GfCustomer customer;
