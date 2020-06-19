80330d3ce707bfa4fef50d5f7a46c10f249044a3
curl123 <pp5066t@gmail.com>
2020-06-19 21:37:58 +0900

    fix conflicts

--- initialization/initialize-magento-categories-english.sh
+++ initialization/initialize-magento-categories-english.sh
@@ -4,15 +4,17 @@ server=$1
 username=$2
 password=$3
 root_id=$4
+storeViewCN=$5
 
 adminToken=$(curl --location --request POST ${server}/index.php/rest/V1/integration/admin/token \
 --header 'Content-Type: application/json' \
 --header 'Cookie: PHPSESSID=f52b8b5466245cc1e0e333f8ae88617f' \
 --data-raw '{
-	"username": "'$username'",
-	"password": "'$password'"
+	"username": "'"$username"'",
+	"password": "'"$password"'"
 }' | jq -r .)
 
+# post to default store view
 PostCategory () {
   echo $(curl --location --request POST ${server}/index.php/rest/V1/categories \
   --header 'Authorization: Bearer '$adminToken'' \
@@ -20,14 +22,36 @@ PostCategory () {
   --header 'Cookie: PHPSESSID=a6d1e4becbed70cbf9c57ba41cf15b5b' \
   --data-raw '{
     "category": {
-      "parent_id": "'$1'",
+      "parent_id": "'"$1"'",
       "name": "'"$2"'",
       "is_active": true,
       "include_in_menu": true,
       "custom_attributes": [
         {
             "attribute_code": "description",
-            "value": "'$3'"
+            "value": "'"$3"'"
+        }
+      ]
+    }
+  }' | jq -r .id)
+}
+
+# post to other store view like cn, en
+PostCategoryWithId () {
+  echo $(curl --location --request POST ${server}/index.php/rest/${1}/V1/categories \
+  --header 'Authorization: Bearer '$adminToken'' \
+  --header 'Content-Type: application/json;charset=utf-8' \
+  --header 'Cookie: PHPSESSID=a6d1e4becbed70cbf9c57ba41cf15b5b' \
+  --data-raw '{
+    "category": {
+      "id": "'"$2"'",
+      "name": "'"$3"'",
+      "is_active": true,
+      "include_in_menu": true,
+      "custom_attributes": [
+        {
+            "attribute_code": "description",
+            "value": "'"$4"'"
         }
       ]
     }
@@ -337,4 +361,3 @@ $(PostCategory $women_boots_id "ankle boots")
 $(PostCategory $women_boots_id "tall boots")
 
 $(PostCategory $women_shoes_id "heels")
-
